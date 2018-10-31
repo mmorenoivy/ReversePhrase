2c0b2306c54a7168449412a23127c8367bd78c04
Marjorie Ivy <morenomt27@gmail.com>
2018-10-31 15:31:08 -0500

    Final code commmit

---
+++ ReversePhrase/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ReversePhrase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x;
+
+            Console.WriteLine("Enter a phrase to reverse: ");
+            x = Console.ReadLine();
+            string text = String.Join(" ", Regex.Split(x, @"(\W+)").Reverse().ToArray());
+             
+
+            Console.WriteLine(text);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
